8c4e582f283e84b34d06597ec08e39150735c603
Lucas Alves de Lima <alvesdelima.lucas45@gmail.com>
2024-12-02 08:02:03 -0300

    feat: add wifi conection getting env in compilation time

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,47 @@
+use embedded_svc::wifi::{ClientConfiguration, Configuration};
+use esp_idf_hal::prelude::Peripherals;
+use esp_idf_svc::{eventloop::EspSystemEventLoop, nvs::EspDefaultNvsPartition, wifi::EspWifi};
+// use esp_idf_sys as _;
+use heapless::String;
+use std::{thread::sleep, time::Duration};
+
 fn main() {
-    // It is necessary to call this function once. Otherwise some patches to the runtime
-    // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
-    esp_idf_svc::sys::link_patches();
-
-    // Bind the log crate to the ESP Logging facilities
+    esp_idf_hal::sys::link_patches(); //Needed for esp32-rs
     esp_idf_svc::log::EspLogger::initialize_default();
+    log::info!("Entered Main function!");
+    let peripherals = Peripherals::take().unwrap();
+    let sys_loop = EspSystemEventLoop::take().unwrap();
+    let nvs = EspDefaultNvsPartition::take().unwrap();
 
-    log::info!("Hello, world!");
+    let mut wifi_driver = EspWifi::new(peripherals.modem, sys_loop, Some(nvs)).unwrap();
+
+    let mut ssid: String<32> = String::try_from(include_str!("../.env/WIFI_SSID")).unwrap();
+    let mut password: String<64> = String::try_from(include_str!("../.env/WIFI_PASSWORD")).unwrap();
+
+    ssid.truncate(ssid.len() - 1);
+    password.truncate(password.len() - 1);
+
+    wifi_driver
+        .set_configuration(&Configuration::Client(ClientConfiguration {
+            ssid,
+            password,
+            ..Default::default()
+        }))
+        .unwrap();
+
+    wifi_driver.start().unwrap();
+    wifi_driver.connect().unwrap();
+    while !wifi_driver.is_connected().unwrap() {
+        let config = wifi_driver.get_configuration().unwrap();
+        log::info!("Waiting for station {:?}", config);
+        sleep(Duration::from_millis(250));
+    }
+    log::info!("Should be connected now");
+    loop {
+        log::info!(
+            "IP info: {:?}",
+            wifi_driver.sta_netif().get_ip_info().unwrap()
+        );
+        sleep(Duration::new(10, 0));
+    }
 }
